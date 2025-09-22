6ceee2b2ed2a10d5ef77f01e5565582cd9d64f5f
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-09-22 11:49:00 +0530

    09-22-2025

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,19 +1,25 @@
 class Solution {
     public int tribonacci(int n) {
-       int t0= 0;
-       int t1= 1;
-       int t2= 1;
-       int t3;
-       if(n==1 || n== 0)
-       {
-        return n;
-       }
-       if(n==2)
-       {
-        return 1;
-       }
-       t3 = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-        return t3;
-       }
-      
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int[] dp = new int[n + 1]; // 1D DP array
+        dp[0] = 0; // T0
+        dp[1] = 1; // T1
+        dp[2] = 1; // T2
+
+        // Fill dp array iteratively
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        return dp[n];
     }
+
+    // Optional test
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 10;
+        System.out.println("Tribonacci(" + n + ") = " + sol.tribonacci(n));
+    }
+}
