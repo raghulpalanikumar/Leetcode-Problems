63a0ba1e6d2d05944d23266768270a94e1974c60
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-09-22 11:45:42 +0530

    09-22-2025

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -3,7 +3,7 @@ class Solution {
        int t0= 0;
        int t1= 1;
        int t2= 1;
-       int t3=0;
+       int t3;
        if(n==1 || n== 0)
        {
         return n;
@@ -12,13 +12,8 @@ class Solution {
        {
         return 1;
        }
-       for(int i=3;i<=n;i++)
-       {
-        t3=t1+t2+t0;
-        t0=t1;
-        t1=t2;
-        t2=t3;
+       t3 = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        return t3;
        }
-       return t3;
+      
     }
-}
