3e0f0102c5f9ebb13cc8610c6c0621c7db9d6be0
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-10-23 10:57:37 +0530

    10-23-2025

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int max=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min)
+            {
+                min=prices[i];
+            }
+            int profit=prices[i]-min;
+            max=Math.max(profit,max);
+        }
+        return max;
+    }
+}
