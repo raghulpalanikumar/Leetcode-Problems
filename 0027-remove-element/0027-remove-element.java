a3fe87e8ce1f9c8c1bc2f6f9e10abb956b702dfd
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-04 17:37:17 +0530

    12-04-2025

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int id = 0; // position to place next valid element
-        
+        int index = 0; // points to the next position to place a valid element
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[id++] = nums[i]; // overwrite elements in-place
+                nums[index] = nums[i];
+                index++;
             }
         }
-        
-        return id; // new length of array without 'val'
+        return index; // return number of elements not equal to val
     }
 }
