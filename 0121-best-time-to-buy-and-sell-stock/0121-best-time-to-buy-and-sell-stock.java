723d6b63371835157fc3c205526ad0e7d6155f74
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-07 23:10:16 +0530

    12-07-2025

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min=prices[0];
-        int max=0;
-        for(int i=0;i<prices.length;i++)
-        {
-            if(prices[i]<min)
-            {
-                min=prices[i];
+        int max = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            // Update the min price if we find a smaller one
+            if (prices[i] < min) {
+                min = prices[i];
             }
-            int profit=prices[i]-min;
-            max=Math.max(profit,max);
+            // Calculate profit with current price and min so far
+            int profit = prices[i] - min;
+            // Keep track of the maximum profit
+            max = Math.max(max, profit);
         }
         return max;
     }
