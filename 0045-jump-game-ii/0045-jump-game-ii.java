9a8a5a224a12370a0f4b10fb548a5a41daa619ad
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-10 07:39:07 +0530

    12-10-2025

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-      int farthest=0;
-      int count=0;
-      for(int i=0;i<nums.length;i++)
-      {
-        if(i>farthest) break;
-        if(nums.length!=1)
-        {
-        farthest =Math.max(farthest,i+nums[i]);
-        count++;
-        if(farthest>=nums.length-1) return count;
-      }
-      }
-      return count;  
+        int farthest = 0;
+        int end = 0;
+        int count = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            
+            // when we reach the end of current jump range
+            if (i == end) {
+                count++;
+                end = farthest;
+            }
+        }
+        return count;
     }
-
 }
