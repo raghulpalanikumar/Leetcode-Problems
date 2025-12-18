c0e518dbe48c1956534960c132807aa1e6f80aa9
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-18 14:31:04 +0530

    12-18-2025

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,46 @@
+class Solution {
+    int count=0;
+    public int findMaxFish(int[][] grid) {
+
+        if (grid == null || grid.length == 0) return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int max = 0;
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (grid[r][c]!=0) {
+                    count=0;
+                    dfs(grid,r,c);
+                    if(count>max)
+                    {
+                        max=count;
+                    }
+                }
+
+            }
+        }
+        return max;
+    }
+
+    void dfs(int[][] grid, int r, int c) {
+
+    
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length
+                || grid[r][c] == 0) {
+            return;
+        }
+
+         count+=grid[r][c];
+        grid[r][c] = 0;
+
+       
+
+    dfs(grid, r, c + 1); // right
+    dfs(grid, r + 1, c); // down
+    dfs(grid, r, c - 1); // left
+    dfs(grid, r - 1, c); // up
+
+    }
+}
