e4bc040770c053ddc18d7f71a7b511de25c53efb
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-22 07:39:47 +0530

    12-22-2025

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,24 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-       String ss=s.trim();
-       String arr[]=ss.split(" ");
-       return arr[arr.length-1].length();
+       // String ss=s.trim();
+       //String arr[]=ss.split(" ");
+       //return arr[arr.length-1].length();
        //another logic is there. back la irunthu va and space irutha neglect and leading space ao
+       int count=0;
+       for(int i=s.length()-1;i>=0;i--)
+       {
+        if(s.charAt(i)==' '&&i==s.length()-1 )
+        {
+            count++;
+        }
+        else if(s.charAt(i)!=' ')
+        {
+            count++;
+        }
+        else{
+            break;
+        }
+       }
+       return count;
     }
 }
