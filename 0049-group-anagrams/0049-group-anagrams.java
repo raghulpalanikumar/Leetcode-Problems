d8eb9f44f8ffcda80e8c2e1e89b37a49a87d82d2
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-23 07:13:06 +0530

    12-23-2025

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,19 +2,27 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
-        for (String word : strs) {
+        for (int i = 0; i < strs.length; i++) {
+            String word = strs[i];
             char[] letters = word.toCharArray();
             Arrays.sort(letters);
             String sortedWord = new String(letters);
 
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+            if (!map.containsKey(sortedWord)){
+                List<String> newList = new ArrayList<>();
+                newList.add(word);
+                map.put(sortedWord, newList);
+            } else {
+                map.get(sortedWord).add(word);
             }
-            map.get(sortedWord).add(word);
         }
+        List<List<String>> result = new ArrayList<>();
 
-        return new ArrayList<>(map.values());
+        for (String key : map.keySet()) {
+            result.add(map.get(key));
+        }
+        return result;
     }
 }
