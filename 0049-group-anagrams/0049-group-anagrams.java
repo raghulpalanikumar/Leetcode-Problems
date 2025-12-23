193cce1fbc90a183856d2051c3b75b63b94f9342
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-23 19:05:16 +0530

    12-23-2025

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,28 +1,29 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (int i = 0; i < strs.length; i++) {
+        Map<String,List<String>> map = new HashMap<>();
+        for(int i=0;i<strs.length;i++)
+        {
             String word = strs[i];
-            char[] letters = word.toCharArray();
-            Arrays.sort(letters);
-            String sortedWord = new String(letters);
-
-            if (!map.containsKey(sortedWord)){
-                List<String> newList = new ArrayList<>();
-                newList.add(word);
-                map.put(sortedWord, newList);
-            } else {
-                map.get(sortedWord).add(word);
+            char letter[]=word.toCharArray();
+            Arrays.sort(letter);
+            String sortedword = new String(letter);
+            
+            if(!map.containsKey(sortedword))
+            {
+                List<String> newlist = new ArrayList<>();
+                newlist.add(word);
+                map.put(sortedword,newlist);
+            }
+            else
+            {
+                map.get(sortedword).add(word);
             }
         }
         List<List<String>> result = new ArrayList<>();
-
-        for (String key : map.keySet()) {
-            result.add(map.get(key));
+        for(String key: map.keySet())
+        {
+         result.add(map.get(key));
         }
         return result;
-    }
+        }
 }
