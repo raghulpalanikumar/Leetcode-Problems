5a37d996b7b2bd3ceabca014084255fe8a08b389
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2025-12-25 06:25:03 +0530

    12-25-2025

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isHappy(int n) {
-    Set<Integer> m = new HashSet<>();
-    while(n!=1)
-    {
-    if(m.contains(n))
-    {
-    return false;
-    }
-    m.add(n);
-    n=trav(n);
-    }
-    return true;
+        Set<Integer> s = new HashSet();
+        while(n!=1)
+        {
+            if(s.contains(n))
+            {
+                return false;
+            }
+        s.add(n);
+        n= trav(n);
+        }
+        return true;
     }
 
     int trav(int n)
@@ -18,9 +18,9 @@ class Solution {
         int sum=0;
         while(n>0)
         {
-         int digit=n%10;
-         sum+=digit*digit;
-         n=n/10;
+        int digit = n%10;
+        sum+= digit*digit;
+        n=n/10;
         }
         return sum;
     }
