9c5f24f905d59c22a7903890b4fa64425247cd62
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2026-01-31 07:32:32 +0530

    01-31-2026

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,31 +13,34 @@
  *     }
  * }
  */
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> q = new ArrayDeque<>();
-        q.add(root);
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.remove();
-                level.add(node.val);
-
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
-            }
-
-            result.add(level);
+        List<List<Integer>> l = new ArrayList<>();
+        if(root == null)
+        {
+            return l;
         }
-
-        return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            List<Integer> r = new ArrayList<>();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode e = q.poll();
+                r.add(e.val);
+                if(e.left!=null)
+                {
+                    q.add(e.left);
+                }
+                if(e.right!=null)
+                {
+                   q.add(e.right);
+                }
+            }
+            l.add(r);
+        }
+        return l;
     }
 }
