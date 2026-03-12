2ff3a757ad99b960ac90f4565a14cc2240e1a312
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2026-03-12 06:13:35 +0530

    03-12-2026

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-    Map<Integer,Integer> m = new HashMap<>();
-    Stack<Integer> s = new Stack<>();
-    for(int i=0;i<nums2.length;i++)
-    {
-        while(!s.isEmpty()&&s.peek()<nums2[i])
+        Map<Integer,Integer> m = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+        for(int i=0;i<nums2.length;i++)
         {
-            m.put(s.pop(),nums2[i]);
+            while(!s.isEmpty() && s.peek()<nums2[i])
+            {
+             m.put(s.pop(),nums2[i]);
+            }
+            s.push(nums2[i]);
         }
-        s.push(nums2[i]);
-    }
-    while(!s.isEmpty())
-    {
-    m.put(s.pop(),-1);
-    }
-    for(int i=0;i<nums1.length;i++)
-    {
-        nums1[i]=m.get(nums1[i]);
-    }
-    return nums1;
+        while(!s.isEmpty())
+        {
+            m.put(s.pop(),-1);
+        }
+        for(int i=0;i<nums1.length;i++)
+        {
+            nums1[i]=m.get(nums1[i]);
+        }
+        return nums1;
     }
 }
