63427bfd0fc80363f05c40e2f3d6763d47eb2dea
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2026-03-17 21:46:25 +0530

    03-17-2026

--- 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.java
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.java
@@ -1,21 +1,18 @@
 class Solution {
     public char kthCharacter(long k, int[] operations) {
-
         int shift = 0;
-
-        for (int i = operations.length - 1; i >= 0; i--) {
-
-            long len = 1L << Math.min(i, 60);   // prevent overflow
-
-            if (k > len) {
-                k -= len;
-
-                if (operations[i] == 1) {
+        for(int i=operations.length-1;i>=0;i--)
+        {
+            long l = 1<<Math.min(i,60);
+            if(k>l)
+            {
+                k-=l;
+                if(operations[i]==1)
+                {
                     shift++;
                 }
             }
         }
-
-        return (char)('a' + (shift % 26));
+        return (char)('a'+(shift%26));
     }
 }
