b46f977adb25f84f06553cfe059d282d9e4bc652
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2026-04-26 18:56:06 +0530

    04-26-2026

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,41 +1,31 @@
 class Solution {
-
+    int count=0;
     public int numIslands(char[][] grid) {
-
-        if (grid == null || grid.length == 0) return 0;
-
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == '1') {
+        int r=grid.length;
+        int c=grid[0].length;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(grid[i][j]=='1')
+                {
                     count++;
-                    dfs(grid, r, c);
+                    dfs(grid,i,j);
                 }
             }
         }
-
         return count;
     }
-
-    // DFS function
-    void dfs(char[][] grid, int r, int c) {
-
-        // base case
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length
-                || grid[r][c] == '0') {
+    void dfs(char grid[][],int r,int c)
+    {
+        if(r<0||c<0||r>=grid.length||c>=grid[0].length||grid[r][c]=='0')
+        {
             return;
         }
-
-        // mark as visited
-        grid[r][c] = '0';
-
-        // explore all 4 directions
-        dfs(grid, r, c + 1); // right
-        dfs(grid, r + 1, c); // down
-        dfs(grid, r, c - 1); // left
-        dfs(grid, r - 1, c); // up
+        grid[r][c]='0';
+        dfs(grid,r+1,c);
+        dfs(grid,r,c+1);
+        dfs(grid,r-1,c);
+        dfs(grid,r,c-1);
     }
 }
