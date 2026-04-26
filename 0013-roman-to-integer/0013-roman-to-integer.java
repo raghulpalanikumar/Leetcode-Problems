a797e526c5a8c29ba585893e12053a581798ad31
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2026-04-26 16:58:48 +0530

    04-26-2026

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,51 +1,51 @@
 class Solution {
     public int romanToInt(String s) {
-        int add=0;
-        int prev=0;
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            int curr=0;
-            char c = s.charAt(i);
-            switch(c)
-            {
-                case 'I':
-                curr=1;
-                break;
+    int sum=0;
+    int prev=0;
+    for(int i=s.length()-1;i>=0;i--)
+    {
+    char c = s.charAt(i);
+    int curr=0;
+    switch(c)
+    {
+    case 'I':
+    curr=1;
+    break;
 
-                case 'V':
-                curr=5;
-                break;
+    case 'V':
+    curr=5;
+    break;
 
-                case 'X':
-                curr=10;
-                break;
+    case 'X':
+    curr=10;
+    break;
 
-                case 'L':
-                curr=50;
-                break;
+    case 'L':
+    curr=50;
+    break;
 
-                case 'C':
-                curr=100;
-                break;
+    case 'C':
+    curr=100;
+    break;
 
-                case 'D':
-                curr=500;
-                break;
+    case 'D':
+    curr=500;
+    break;
 
-                case 'M':
-                curr=1000;
-                break;
-            }
-            if(curr>=prev)
-            {
-                add+=curr;
-            }
-            else
-            {
-                add-=curr;
-            }
-            prev=curr;
-        }
-        return add;
+    case 'M':
+    curr=1000;
+    break;
+    }
+    if(curr>=prev)
+    {
+    sum+=curr;
+    }
+    else
+    {
+    sum-=curr;
+    }
+    prev=curr;
+    }
+    return sum;
     }
 }
