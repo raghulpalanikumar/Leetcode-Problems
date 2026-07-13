f7d8a17dd9f503bab76c6be4878a5e767cd5ddba
Raghul P <155134503+raghulpalanikumar@users.noreply.github.com>
2026-07-13 19:09:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (94.86%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,31 +1,21 @@
 class Solution {
-    public List<Integer> sequentialDigits(int low, int high) {
-
-        List<Integer> ans = new ArrayList<>();
-
-        // Base string containing all sequential digits
-        String digits = "123456789";
-
-        // Number of digits in low and high
-        int lowLength = String.valueOf(low).length();
-        int highLength = String.valueOf(high).length();
-
-        // Generate numbers of every possible length
-        for (int len = lowLength; len <= highLength; len++) {
-
-            // Generate all sequential numbers of this length
-            for (int i = 0; i + len <= 9; i++) {
-
-                // Take a substring and convert it to an integer
-                int num = Integer.parseInt(digits.substring(i, i + len));
-
-                // Add only if it lies within the range
-                if (num >= low && num <= high) {
-                    ans.add(num);
-                }
-            }
+    public List<Integer> sequentialDigits(int loww, int highh) {
+    List<Integer> l = new ArrayList<>();
+    String digit = "123456789";
+    int low = String.valueOf(loww).length();
+    int high = String.valueOf(highh).length();
+    for(int i=low;i<=high;i++)
+    {
+        int t=0;
+        for(int j=i;j<=9;j++)
+        {   
+        int num = Integer.parseInt(digit.substring(t++,j));
+        if(num>=loww && num<=highh)
+        {
+            l.add(num);
         }
-
-        return ans;
+        }
+    }
+    return l;
     }
 }
